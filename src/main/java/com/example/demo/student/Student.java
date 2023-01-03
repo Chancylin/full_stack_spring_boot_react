ad67c288ddb5c962e3a49c281366bb4542bfcfe1
Chuangxin Lin <lcxustc@gmail.com>
2023-01-02 23:58:36 -0500

    Chap9: Server and Client Error Handling. Enforcing Database Constraints.

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -30,9 +30,11 @@ public class Student {
     @NotBlank
     private String name;
     @Email
+    @Column(nullable = false, unique = true)
     private String email;
     @NotNull
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Gender gender;
 
 }
