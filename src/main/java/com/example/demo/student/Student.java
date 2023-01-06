7b1876611ed392c46cda4350c10b6c9f38d17004
Chuangxin Lin <lcxustc@gmail.com>
2023-01-06 12:42:00 -0500

    Chap12: Testing. Added unit test for StudentRepository.

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -37,4 +37,9 @@ public class Student {
     @Column(nullable = false)
     private Gender gender;
 
+    public Student(String name, String email, Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
 }
