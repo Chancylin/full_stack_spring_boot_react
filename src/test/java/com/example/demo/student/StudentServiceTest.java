f90da742fbfe49f35a9af17dc4dcb80bed2df591
Chuangxin Lin <lcxustc@gmail.com>
2023-01-06 18:06:14 -0500

    Chap12: Testing. Added Unit test for StudentService; ran with coverage; now covered the scenario that student exist (throw Exception) for addStudent().

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import com.example.demo.student.exception.BadRequestException;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -7,6 +8,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,6 +55,27 @@ class StudentServiceTest {
 
         Student capturedStudent = studentArgumentCaptor.getValue();
         assertThat(capturedStudent).isEqualTo(student); // check the values are the same
+    }
+
+    @Test
+    void willThrowWhenEmailIsTaken() {
+        // given
+        String email = "jamila@gmail.com";
+        Student student = new Student(
+                "Jamila",
+                email,
+                Gender.FEMALE
+        );
+
+        given(studentRepository.selectExistsEmail(student.getEmail())) // or we can use anySting() here
+                .willReturn(true);
+        // when
+        // then
+        assertThatThrownBy(() -> underTest.addStudent(student))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("Email " + student.getEmail() + " taken");
+
+        verify(studentRepository, never()).save(any());
     }
 
     @Test
