13a8b1188f82f44a19b90bf22a39c65cb5a9c2ba
Chuangxin Lin <lcxustc@gmail.com>
2023-01-07 22:27:52 -0500

    Chap12: Testing. Integration test: use MockMvc to test against our Controller.

--- src/test/java/com/example/demo/integration/StudentIT.java
+++ src/test/java/com/example/demo/integration/StudentIT.java
@@ -1,16 +1,59 @@
 package com.example.demo.integration;
 
+import com.example.demo.student.Gender;
+import com.example.demo.student.Student;
+import com.example.demo.student.StudentRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @TestPropertySource(
         locations = "classpath:application-it.properties"
 )
+@AutoConfigureMockMvc
 public class StudentIT {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
     @Test
-    void name() {
+    void canRegisterNewStudent() throws Exception {
+        // given
+        Student student = new Student(
+                "Jamila",
+                "lol@gmail.com",
+                Gender.FEMALE);
+
+        // when
+        ResultActions resultActions = mockMvc
+                .perform(post("/api/v1/students")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(student))); // student is what we want to send
+        // then
+        resultActions.andExpect(status().isOk());
+
+        List<Student> students = studentRepository.findAll();
+        assertThat(students)
+                .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
+                .contains(student);
     }
 }
