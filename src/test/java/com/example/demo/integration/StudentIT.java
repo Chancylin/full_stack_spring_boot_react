27848bea9d115ffa2d09d432f9ca42025ebd86c0
Chuangxin Lin <lcxustc@gmail.com>
2023-01-07 23:25:48 -0500

    Chap12: Testing. Integration test: added test for deleteStudent.

--- src/test/java/com/example/demo/integration/StudentIT.java
+++ src/test/java/com/example/demo/integration/StudentIT.java
@@ -3,22 +3,25 @@ package com.example.demo.integration;
 import com.example.demo.student.Gender;
 import com.example.demo.student.Student;
 import com.example.demo.student.StudentRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import com.github.javafaker.Internet;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -63,5 +66,63 @@ public class StudentIT {
         assertThat(students)
                 .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
                 .contains(student);
+    }
+
+    @Test
+    void canDeleteExistStudent() throws Exception {
+        // add a new student
+        String name = String.format("%s %s",
+                faker.name().firstName(),
+                faker.name().lastName()
+        );
+
+        String email = name.split(" ")[1] + "@amigoscode.edu";
+
+        Student student = new Student(
+                name,
+                email,
+                Gender.FEMALE);
+
+        mockMvc.perform(post("/api/v1/students")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(student))) // student is what we want to send
+                .andExpect(status().isOk());
+
+        // get all students
+        MvcResult getStudentsResult = mockMvc.perform(get("/api/v1/students")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String contentAsString = getStudentsResult
+                .getResponse()
+                .getContentAsString();
+
+        // map to list of objects
+        List<Student> students = objectMapper.readValue(
+                contentAsString,
+                new TypeReference<>() {
+                }
+        );
+
+        // get the studentId
+        long id = students.stream()
+                .filter(s -> s.getEmail().equals(student.getEmail()))
+                .map(Student::getId)
+                .findFirst()
+                .orElseThrow(() ->
+                        new IllegalStateException(
+                                "student with email: " + email + " not found"
+                        )
+                );
+
+        // when
+        ResultActions resultActions =
+                mockMvc.perform(delete("/api/v1/students/" + id));
+
+        // then
+        resultActions.andExpect(status().isOk());
+        boolean exists = studentRepository.existsById(id);
+        assertThat(exists).isFalse(); // if deleted, should be false
     }
 }
